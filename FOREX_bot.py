3a887997234d9bea451634b6ee36326d3c682d21
IMaksakov <105012075+IMaksakov@users.noreply.github.com>
2022-07-31 15:07:01 +0300

    Правка ошибки в коде

--- FOREX_bot.py
+++ FOREX_bot.py
@@ -11,24 +11,29 @@ bot = telebot.TeleBot(TOKEN)
 #приветствие
 @bot.message_handler(commands= ["start", "help"])
 def greetings (message: telebot.types.Message):
-    test = "Привет!"
+    text = "Привет! Я умею конвертировать валюты" '\n'\
+           "Введи 3 параметра через пробел:" '\n' \
+           "Валюту, которую ты хочешь конвертировать" '\n' \
+           "Валюту, в которую хочешь перевести" '\n' \
+           "Объем валюты на перевод" '\n' \
+           "Например: доллар евро 15"
     bot.send_message(message.chat.id, text)
 
 #список валют
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = "Я умею конвертировать:"
-    for key in exchanges.keys():
+    for key in keys.keys():
         text = '\n'.join((text, key, ))
     bot.reply_to(message, text)
-    
+
 #отлавливание ошибок
 @bot.message_handler(content_types=['text'])
 def converter(message: telebot.types.Message):
     values = message.text.split(' ')
     try:
         if len(values) != 3:
-            raise APIException('Некорректный ввод - пересмотри число переменных)')
+            raise APIException('Некорректный ввод, введи команду /help')
         answer = Converter.get_price(*values)
     except APIException as e:
         bot.reply_to(message, f"Ошибка в команде. \n{e}")
